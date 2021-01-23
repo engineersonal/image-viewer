4c60c8ad9f6aca95aa0d1f1ef05c25274fdc3b0d
Sonal <engineersonal@rediffmail.com>
2021-01-23 18:31:14 +0530

    Added Profile Component and Router Setup

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./home/Home";
 import Login from "./login/Login";
+import Profile from "./profile/Profile";
 
 //Creating controller class for easy routing the pages
 class Controller extends Component {
@@ -27,6 +28,12 @@ class Controller extends Component {
             render={(props) => <Home {...props} baseUrl={this.baseUrl} />}
           />
           {/* Route to home Page */}
+          <Route
+            exact
+            path="/profile"
+            render={(props) => <Profile {...props} baseUrl={this.baseUrl} />}
+          />
+          {/* Route to profile Page */}
         </div>
       </Router>
     );
